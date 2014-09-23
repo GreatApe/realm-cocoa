11a1ed04fa998aef94f56f4b6cc9911fddd6431b
Gustaf Kugelberg <kugel@kth.se>
2014-09-23 17:45:43 +0200

    First commit

---
+++ tools/RealmBrowser/RealmBrowser/Classes/RLMSplashWindowController.h
@@ -0,0 +1,13 @@
+//
+//  RLMSplashWindowController.h
+//  RealmBrowser
+//
+//  Created by Gustaf Kugelberg on 23/09/14.
+//  Copyright (c) 2014 Realm inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RLMSplashWindowController : NSWindowController
+
+@end
---
+++ tools/RealmBrowser/RealmBrowser/Classes/RLMSplashWindowController.m
@@ -0,0 +1,23 @@
+//
+//  RLMSplashWindowController.m
+//  RealmBrowser
+//
+//  Created by Gustaf Kugelberg on 23/09/14.
+//  Copyright (c) 2014 Realm inc. All rights reserved.
+//
+
+#import "RLMSplashWindowController.h"
+
+@interface RLMSplashWindowController ()
+
+@end
+
+@implementation RLMSplashWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
